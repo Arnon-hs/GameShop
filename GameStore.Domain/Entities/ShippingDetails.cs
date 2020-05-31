232bfb09fafbf6fce35ea1353aeeef3d57f6a537
Vasiliy <vasya09082001@gmail.com>
2020-06-01 02:30:29 +0300

    final push

--- GameStore.Domain/Entities/ShippingDetails.cs
+++ GameStore.Domain/Entities/ShippingDetails.cs
@@ -7,12 +7,13 @@ namespace GameStore.Domain.Entities
         [Required(ErrorMessage = "Укажите как вас зовут")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Укажите email")]
+        [Display(Name = "Email")]
+        public string Line2 { get; set; }
+
         [Required(ErrorMessage = "Вставьте первый адрес доставки")]
         [Display(Name="Первый адрес")]
         public string Line1 { get; set; }
-
-        [Display(Name = "Второй адрес")]
-        public string Line2 { get; set; }
 
         [Required(ErrorMessage = "Укажите город")]
         [Display(Name = "Город")]
